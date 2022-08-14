5710535d69ee4790058bbbb5b93e754570f40aab
Kushagra Sharma <sparrow.kushagra@gmail.com>
2022-08-15 05:17:54 +0530

    removed runtime version

--- Backend/settings.py
+++ Backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(%slrtpn4$7)n6(!mtf9*o5+=jaa!cx!=^@d7_h#r&y75!aip$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tle-backend.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
