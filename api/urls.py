f3c3b012d3dd2eb8926845575db4357d20b4158c
Kushagra Sharma <sparrow.kushagra@gmail.com>
2022-08-25 10:34:46 +0530

    added endpoint for orbital elements

--- api/urls.py
+++ api/urls.py
@@ -5,11 +5,11 @@ from .views import RegisterUserAPIView
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('index/',views.index),
     path('register/',RegisterUserAPIView.as_view()),
     path('gettle/', views.get_tle),
     path('tlebyname/<str:name>/', views.tle_by_name),
     path('tlebyid/<str:id>/', views.tle_by_id),
+    path('orbital_elements/<str:satName>/', views.orbital_elements),
     path('satellite/', views.satellite_list),
     path('satellite/<name>', views.satellite_detail),
     path('satellite/<name>/<sensor>', views.sensor_detail),
